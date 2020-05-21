2f55a05bf990c444e62ac09a5cee7ef7789d4bc1
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2020-05-20 22:21:15 -0500

    Add wikirandom command

--- boogabot.py
+++ boogabot.py
@@ -2,6 +2,9 @@
 
 import os
 import random
+import requests
+import urllib.parse
+
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -9,6 +12,8 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
+
+# Regular commands
 
 
 @bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
@@ -33,6 +38,15 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
         for _ in range(number_of_dice)
     ]
     await ctx.send(', '.join(dice))
+
+
+@bot.command(name='wikirandom', help='Posts a random wikipedia page.')
+async def random_wiki(ctx):
+    r = requests.head(
+        'https://en.wikipedia.org/wiki/Special:Random', allow_redirects=True)
+    await ctx.send(print(urllib.parse.unquote(r.url)))
+
+# Admin commands
 
 
 @bot.command(name='create-channel')
@@ -64,13 +78,28 @@ async def on_message(message):
     replies = {
         'ooga': 'Booga',
         'epic': 'WOW',
+        'oof': 'Thanks for contributing nothing to the conversation.'
     }
 
-    for key in replies:
-        if message.content.lower() == key:
-            await message.channel.send(replies[key])
-        elif ''.join(message.content.lower().split(' ')) == key:
-            await message.channel.send(replies[key])
+    anime_replies = [
+        "https://tenor.com/view/really-anime-seriously-kiruya-momochi-karyl-anime-faces-gif-17024702",
+        "https://tenor.com/view/cal-princess-connect-disgusting-cry-gif-16950076",
+        "https://tenor.com/view/princess-connect-re-dive-anime-smile-thumbs-up-gif-17119800",
+        "https://tenor.com/view/karyl-kyaru-princess-connect-re-dive-anime-blushing-gif-16984580",
+        "https://tenor.com/view/karyl-kyaru-princess-connect-re-dive-anime-gif-16984595",
+        "https://tenor.com/view/karyl-kyaru-princess-connect-re-dive-anime-gif-16984597",
+        "https://tenor.com/view/karyl-kyaru-princess-connect-re-dive-anime-gif-16984575"
+    ]
 
+    for reply_key in replies:
+        if message.content.lower() == reply_key:
+            await message.channel.send(replies[reply_key])
+        elif ''.join(message.content.lower().split(' ')) == reply_key:
+            await message.channel.send(replies[reply_key])
+
+    if ''.join(message.content.lower().split(' ')) == "animewasamistake":
+        await message.channel.send(anime_replies[random.randrange(0, len(anime_replies))])
+
+    await bot.process_commands(message)
 
 bot.run(TOKEN)
