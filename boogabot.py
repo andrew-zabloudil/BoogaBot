df88bef379f5810e2daafae8a454f8efd6109e90
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2020-05-24 00:15:27 -0500

    Fix API Token

--- boogabot.py
+++ boogabot.py
@@ -11,8 +11,8 @@ from cogs.admin_commands import AdminCommands
 from cogs.bot_listeners import BotListeners
 
 load_dotenv()
-# TOKEN = os.getenv('DISCORD_TOKEN')
-TOKEN = os.getenv('DISCORD_TEST_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
+# TOKEN = os.getenv('DISCORD_TEST_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 bot.add_cog(RegularCommands(bot))
