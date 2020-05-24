27011f4377d66dde5296a9280ed5d42e32c3c38c
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2020-05-23 21:27:43 -0500

    Improve file structure

--- boogabot.py
+++ boogabot.py
@@ -1,24 +1,21 @@
 #! python3
 
 import os
-import random
 from dotenv import load_dotenv
-
 
 import discord
 from discord.ext import commands
 
-from bot_commands import regular_commands
-from bot_commands import admin_commands
-from bot_commands import bot_listeners
+from cogs.regular_commands import RegularCommands
+from cogs.admin_commands import AdminCommands
+from cogs.bot_listeners import BotListeners
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 # TOKEN = os.getenv('DISCORD_TEST_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
-
-bot.add_cog(regular_commands.RegularCommands(bot))
-bot.add_cog(admin_commands.AdminCommands(bot))
-bot.add_cog(bot_listeners.BotListeners(bot))
+bot.add_cog(RegularCommands(bot))
+bot.add_cog(AdminCommands(bot))
+bot.add_cog(BotListeners(bot))
 bot.run(TOKEN)
