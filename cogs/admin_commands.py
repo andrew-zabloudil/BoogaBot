851ec0be07a58ab60f9713b6f746cf8b76d3d383
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2020-05-26 21:44:51 -0500

    Add kick and ban commands

--- cogs/admin_commands.py
+++ cogs/admin_commands.py
@@ -17,3 +17,39 @@ class AdminCommands(commands.Cog):
         if not existing_channel:
             print(f'Creating a new channel: {channel_name}')
             await guild.create_text_channel(channel_name)
+
+    @commands.command(name='kick', help='ADMIN: Kicks the specified user.')
+    @commands.has_permissions(kick_members=True)
+    async def kick_user(self, ctx, user_name=None, reason=None):
+        guild = ctx.guild
+
+        if user_name:
+            for member in guild.members:
+                if user_name == member.display_name:
+                    await member.kick(reason=reason)
+                    if reason:
+                        await ctx.send(f'Kicked {user_name} for {reason}.')
+                    else:
+                        await ctx.send(f'Kicked {user_name}.')
+                    return
+            await ctx.send('That user does not exist.')
+        else:
+            await ctx.send('You must input a valid user.')
+
+    @commands.command(name='ban', help='ADMIN: Bans the specified user.')
+    @commands.has_permissions(ban_members=True)
+    async def ban_user(self, ctx, user_name=None, reason=None, days=None):
+        guild = ctx.guild
+
+        if user_name:
+            for member in guild.members:
+                if user_name == member.display_name:
+                    await member.ban(reason=reason, delete_message_days=days)
+                    if reason:
+                        await ctx.send(f'Banned {user_name} for {reason}.')
+                    else:
+                        await ctx.send(f'Banned {user_name}.')
+                    return
+            await ctx.send('That user does not exist.')
+        else:
+            await ctx.send('You must input a valid user.')
