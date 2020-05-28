d77b3da754728dc307453cd3a18e984c1343b22e
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2020-05-27 23:04:47 -0500

    Add TheHill to covid-news, Optimized code

--- cogs/regular_commands.py
+++ cogs/regular_commands.py
@@ -4,6 +4,7 @@ import random
 import discord
 import requests
 import urllib
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 from discord.ext import commands
 
@@ -68,96 +69,108 @@ class RegularCommands(commands.Cog):
 
         await ctx.send(embed=covid_embed)
 
-    @commands.command(name="covid-news", help="Displays COVID-19 news. (Mixed, BBC, or Reuters)")
+    @commands.command(name="covid-news", help="Displays COVID-19 news. (Mixed, BBC, Reuters, NPR, The Hill)")
     async def covid_news(self, ctx, source="mixed"):
 
+        # Checks the source against the dictionaries for expected misspellings, etc.
         source = source.lower()
+        sources = {
+            "mixed": ("mixed", "mix", "all"),
+            "bbc": ("bbc"),
+            "reuters": ("reuters", "reuter", "reuter's", "rueters", "rueter's"),
+            "npr": ("npr"),
+            "the hill": ("thehill", "the hill", "the-hill", "the_hill", "the")
+        }
+        for site, spelling in sources.items():
+            if source in spelling:
+                source = site
+
+        # Lists the proper display names, urls, html tags, and colors to be used for the scraping and embedding.
         names = {
             "bbc": "BBC",
             "reuters": "Reuters",
-            "npr": "NPR"
+            "npr": "NPR",
+            "the hill": "The Hill"
         }
         urls = {
             "bbc": "https://www.bbc.com/news/coronavirus",
             "reuters": "https://www.reuters.com/live-events/coronavirus-6-id2921484",
-            "npr": "https://www.npr.org/sections/coronavirus-live-updates"
+            "npr": "https://www.npr.org/sections/coronavirus-live-updates",
+            "the hill": "https://thehill.com/social-tags/coronavirus"
         }
         html_tags = {
-            "bbc": ("a", {"class": "qa-heading-link"}),
-            "reuters": ("div", {"class": "FeedBox__container___3wxiT item-container LiveBlogStreamPage-post-3KSe2"}),
-            "npr": ("div", {"class": "item-info"})
+            "bbc": ("h3", {"class": "lx-stream-post__header-title gel-great-primer-bold"}, "a"),
+            "reuters": ("div", {"class": "FeedBox__container___3wxiT item-container LiveBlogStreamPage-post-3KSe2"}, "a"),
+            "npr": ("div", {"class": "item-info"}, "a"),
+            "the hill": ("h2", {"class": "node__title node-title"}, "a")
         }
-
-        # html_tags = {
-        #     "bbc": ("a", {"class": "qa-heading-link"}),
-        #     "reuters": ("div", {
-        #         "class": "FeedBox__container___3wxiT item-container LiveBlogStreamPage-post-3KSe2"}),
-        #     "npr": ("div", {"class": "item-info"})
-        # }
         colors = {
+            "mixed": 0xd22188,
             "bbc": 0xbb1919,
             "reuters": 0xfb8033,
-            "npr": 0x237bbd
+            "npr": 0x237bbd,
+            "the hill": 0x2a53c1
         }
 
+        # Checks whether the feed being built will come from a single source or be a mixed feed from all sources.
         if source == "mixed":
             links = {}
+            url = 'https://www.cdc.gov/coronavirus/2019-ncov/index.html'
             for site in urls:
                 r = requests.get(urls[site])
                 soup = BeautifulSoup(r.text, "html.parser")
                 links[site] = soup.findAll(
                     html_tags[site][0], html_tags[site][1])
-            color = 0xd22188
-
         else:
             url = urls[source]
             r = requests.get(url)
             soup = BeautifulSoup(r.text, "html.parser")
             links = soup.findAll(html_tags[source][0], html_tags[source][1])
-            color = colors[source]
 
+        # Makes the base Discord embed that the articles will be added to.
         covid_news_embed = discord.Embed(
             title=f"COVID-19 {source.upper()} LIVE NEWS",
-            color=color
+            url=url,
+            color=colors[source]
         )
 
-        if source != "mixed":
+        # Builds the news feed.
+        if source == "mixed":
+            for i in range(0, 10, len(urls)):
+                for site in names:
+                    try:
+                        value_text = links[site][i].find(
+                            html_tags[site][2]).text
+                        value_link = urljoin(
+                            urls[site],
+                            links[site][i].find(
+                                html_tags[site][2]).attrs["href"]
+                        )
+                        covid_news_embed.add_field(
+                            name=names[site],
+                            value=f'[{value_text}]({value_link})',
+                            inline=False
+                        )
+                    except:
+                        continue
+        else:
             for i in range(min(10, len(links))):
                 try:
-                    if source == "bbc":
-                        name = names[source]
-                        value = f'[{links[i].find("span").text}](https://www.bbc.com{links[i].attrs["href"]})'
-                    else:
-                        name = names[source]
-                        value = f'[{links[i].find("a").text}]({links[i].find("a").attrs["href"]})'
+                    value_text = links[i].find(html_tags[source][2]).text
+                    value_link = urljoin(
+                        urls[source],
+                        links[i].find(html_tags[source][2]).attrs["href"]
+                    )
                 except:
                     continue
 
                 covid_news_embed.add_field(
-                    name=name,
-                    value=value,
+                    name=names[source],
+                    value=f'[{value_text}]({value_link})',
                     inline=False
                 )
 
-        if source == "mixed":
-            for i in range(int(10 / len(urls))):
-                for site in names:
-                    try:
-                        if site == "bbc":
-                            covid_news_embed.add_field(
-                                name=names[site],
-                                value=f'[{links[site][i].find("span").text}](https://www.bbc.com{links[site][i].attrs["href"]})',
-                                inline=False
-                            )
-                        else:
-                            covid_news_embed.add_field(
-                                name=names[site],
-                                value=f'[{links[site][i].find("a").text}]({links[site][i].find("a").attrs["href"]})',
-                                inline=False
-                            )
-                    except:
-                        continue
-
+        # Sends the fully built embed to Discord.
         await ctx.send(embed=covid_news_embed)
 
     @commands.command(name="gunpla-news", help="Displays the latest Gunpla news.")
