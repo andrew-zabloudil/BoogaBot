42640bcf44e3e3b202adf3c7ed3153c248314ab4
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2020-06-18 00:39:14 -0500

    Change admin commands to mod commands

--- boogabot.py
+++ boogabot.py
@@ -7,15 +7,14 @@ import discord
 from discord.ext import commands
 
 from cogs.regular_commands import RegularCommands
-from cogs.admin_commands import AdminCommands
+from cogs.mod_commands import ModCommands
 from cogs.bot_listeners import BotListeners
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-# TOKEN = os.getenv('DISCORD_TEST_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 bot.add_cog(RegularCommands(bot))
-bot.add_cog(AdminCommands(bot))
+bot.add_cog(ModCommands(bot))
 bot.add_cog(BotListeners(bot))
 bot.run(TOKEN)
