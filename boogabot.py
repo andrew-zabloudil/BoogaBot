257578c00edf6c9908474f79e7546e2e03861584
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2020-06-21 12:27:17 -0500

    Remove Dadbot from current master

--- boogabot.py
+++ boogabot.py
@@ -9,7 +9,6 @@ from discord.ext import commands
 from cogs.regular_commands import RegularCommands
 from cogs.mod_commands import ModCommands
 from cogs.bot_listeners import BotListeners
-from cogs.dadbot import Dadbot
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -18,6 +17,5 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
 bot.add_cog(RegularCommands(bot))
 bot.add_cog(ModCommands(bot))
 bot.add_cog(BotListeners(bot))
-bot.add_cog(Dadbot(bot))
 
 bot.run(TOKEN)
