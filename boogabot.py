78aa114193b99e49596ab0ea97fa1fb8245787dc
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2020-06-20 23:53:56 -0500

    Add dad joke machine learning bot

--- boogabot.py
+++ boogabot.py
@@ -9,12 +9,15 @@ from discord.ext import commands
 from cogs.regular_commands import RegularCommands
 from cogs.mod_commands import ModCommands
 from cogs.bot_listeners import BotListeners
+from cogs.dadbot import Dadbot
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
 
 bot.add_cog(RegularCommands(bot))
 bot.add_cog(ModCommands(bot))
 bot.add_cog(BotListeners(bot))
+bot.add_cog(Dadbot(bot))
+
 bot.run(TOKEN)
